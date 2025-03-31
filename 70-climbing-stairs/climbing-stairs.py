7384a0a4fa62cd451d8666354e3707f449fcb0e9
Doğukan Süme <161054688+CoderRoninn@users.noreply.github.com>
2025-03-31 23:26:44 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (78.63%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,21 +1,24 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
-        if n <= 2:
-            return n
-
         prev1, prev2 = 1, 2
-        current = 0
+        next = None
+
+        if n == 1:
+            return 1
+
+        if n == 2:
+            return 2
 
         for _ in range(2,n):
-            current = prev1 + prev2
+            next = prev1 + prev2
             prev1 = prev2
-            prev2 = current
-        return current    
+            prev2 = next
 
+        return next   
 
-        
-        
-            
+        #Time complexity of this algorithm is O(n) because we iterate thought the loop once 
+        #The loop runs from 2 to n  where n is input number
+        #Time space of this algoirthm is O(1) because we don't use any additional data structure. we used only constant amount variable 
 
+                
         
