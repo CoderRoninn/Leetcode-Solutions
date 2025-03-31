37f58771f3672bb555a021bd6d6ccd06375b5386
Doğukan Süme <161054688+CoderRoninn@users.noreply.github.com>
2025-03-31 21:51:45 +0300

    Time: 46 ms (38.54%) | Memory: 17.6 MB (89.31%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,15 +1,23 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n <= 0:
-            return 0
-        elif n == 1:
-            return 1
-        t1, t2, next = 0, 1, 0
 
-        for i in range(2, n+1):
+        t1, t2 = 0, 1
+        next = None
+
+        if n == 0: #Base case
+            return 0
+
+        if n == 1: # Base case
+            return 1
+
+        for _ in range(2, n+1):
             next = t1 + t2
             t1 = t2
             t2 = next
-        return next
-        
+        return next   
+
+        #Time complexity of this algorithm is O(n) because we iterate through the loop once where n is the input number
+        #The loop runs linearly from 2 to n where n is input number
+        #Space complexity of this algorithm is O(1) because we don't use any additional data structure. we only use constant amount of variable    
+               
         
