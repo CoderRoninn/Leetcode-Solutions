b8330896df192de1791c2e6e82b8dc49d9e3b844
Doğukan Süme <161054688+CoderRoninn@users.noreply.github.com>
2025-04-10 01:14:57 +0300

    Time: 4 ms (70.62%) | Memory: 19.8 MB (5.27%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,13 +1,19 @@
+from collections import Counter
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
 
-        count, target = 0, None
+        freq = Counter(nums)
+        return max(freq.items(), key=lambda x: x[1])[0]
 
-        for num in nums:
-            if count == 0:
-                target = num
-            count += 1 if target == num else -1  
-        return target    
+       
 
-            #Boyer-Moore Voting Algorithm  
+
+        #Time complexity of this algorithm is O(n) because we iterate through the input array once to count frequency of each element  where n is the length of imput array
+        #Also finding max element takes O(n) where n is the length of imput array
+        #Space complexity of this algorithm is O(n) because we created a dictionary(Counter) of length n, where n is the length of input array
+
+
+
+        
         
