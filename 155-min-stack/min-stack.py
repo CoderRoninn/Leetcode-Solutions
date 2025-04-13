cd664cd6385c3230f1e9e52ebbff0ea2d6d91846
Doğukan Süme <161054688+CoderRoninn@users.noreply.github.com>
2025-04-13 21:36:57 +0300

    Time: 14 ms (15.05%) | Memory: 21.4 MB (43.34%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -2,37 +2,43 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
-        self.min_stack = []
+        self.min_stack = [] # To keep min value in the stack
+
         
-    def push(self, val: int) -> None:
+
+    def push(self, val: int) -> None: #O(1)
         self.stack.append(val)
 
         if not self.min_stack or val <= self.min_stack[-1]:
             self.min_stack.append(val)
-        
+        # Normally finding min value in the stack takes O(n)
+        # But by means of min_stack method we can find with O(1)
 
-    def pop(self) -> None:
+    def pop(self) -> None: #O(1)
         if self.stack:
-            top_val =self.stack.pop()
-
-        if top_val == self.min_stack[-1]:
-            self.min_stack.pop()    
+            top_value = self.stack.pop()
+        if top_value == self.min_stack[-1]:
+            self.min_stack.pop()   
+        else:
+            print("The stack is empty, so you do not implement this operation.")    
         
 
-    def top(self) -> int:
+    def top(self) -> int: # O(1)
         if self.stack:
             return self.stack[-1]
-
         else:
-            print("There is no element to show")    
+            print("The stacj is empty, so you do not implement this operation.")    
+        
         
 
     def getMin(self) -> int:
         if self.min_stack:
             return self.min_stack[-1]
-
         else:
-            print("There is no element to show")    
+            print("The stack is empty so, you do not implement this operation") 
+    
+    #Time complexity of this algorithm is O(1) because every function takes O(1)
+    #Space complexity of this algorithm is O(n) where n is the number of elements in the stack          
         
 
 
